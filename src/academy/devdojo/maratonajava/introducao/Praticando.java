0b466b5c8ec571df538a9deb7d7f9b33ebdf7201
Pablo Matos Lima <pablomtlima1@gmail.com>
2023-09-08 10:19:57 -0300

    first commit

--- src/academy/devdojo/maratonajava/introducao/Praticando.java
+++ src/academy/devdojo/maratonajava/introducao/Praticando.java
@@ -1,2 +1,46 @@
-package academy.devdojo.maratonajava.introducao;public class Praticando {
+package academy.devdojo.maratonajava.introducao;
+
+public class Praticando {
+    public static void main(String[] args) {
+
+        /* Write a Java program to print the results of the following operations.
+            Test Data:
+            a. -5 + 8 * 6
+            b. (55+9) % 9
+            c. 20 + -3*5 / 8
+            d. 5 + 15 / 3 * 2 - 8 % 3
+            Expected Output :
+            43
+            1
+            19
+            13
+
+        Resolution
+
+        int a = -5 + 8 * 6;
+        int b = (55 + 9) % 9;
+        int c = 20 + (-3 * 5 / 8);
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        */
+
+        /* Write a Java program to display the following pattern.
+        * <p> Sample Pattern :</p>
+        *    J    a   v     v  a
+        *    J   a a   v   v  a a
+        * J  J  aaaaa   V V  aaaaa
+        *  JJ  a     a   V  a     a
+        * solution
+        * System.out.println("    J    a   v     v  a");
+        * System.out.println("    J   a a   v   v  a a");
+        * System.out.println(" J  J  aaaaa   V V  aaaaa");
+        * System.out.println("  JJ  a     a   V  a     a");
+        */
+
+        System.out.println("OI");
+    }
 }
