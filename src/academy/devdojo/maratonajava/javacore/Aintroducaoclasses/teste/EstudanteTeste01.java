d4c63e0b012de35366dc6714d0717634728ca094
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2024-09-24 21:49:49 -0300

    Aula 40, 41 Ok

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTeste01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        Estudante estudante1 = new Estudante();
+
+        estudante.nome = "Pablo";
+        estudante.idade = 27;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/teste/EstudanteTeste02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.nome);
+    }
+}
